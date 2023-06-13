28838a83763529777a1ddb516d2e40df24d22c58
Vipul Singh <120732585+Vipul1305@users.noreply.github.com>
2023-06-14 02:48:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	arr.sort();
+	let n = arr.length;
+	let i = 0 ; j = n-1;
+	let max = Number.MAX_VALUE;
+	let ans = 0
+	while (i<j) {
+		let k = i+1;
+		
+		while(k<j){
+			let sum = arr[i] + arr[k] + arr[j];
+			if(sum < target){
+				k++;
+			}else if(sum > target){
+				j--;
+			}else{
+				return sum;
+			}
+			let diff = math.abs(sum - target);
+			if (diff<max){
+				max = diff;
+				ans = sum;
+			}
+		}
+		i++;
+	}
+	return ans;
 }
 
 module.exports = threeSum;
